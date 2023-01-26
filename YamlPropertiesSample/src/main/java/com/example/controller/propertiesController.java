b18ab9f0e2efc3cee6443cf4c46e50f11dcd59cc
yukishi24 <yukishi24github@gmail.com>
2023-01-26 23:40:36 +0900

    add: yaml properties config file -> bug

--- YamlPropertiesSample/src/main/java/com/example/controller/propertiesController.java
+++ YamlPropertiesSample/src/main/java/com/example/controller/propertiesController.java
@@ -1,12 +1,20 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.config.YamlPropertiesConfig;
 
 @Controller
 public class propertiesController {
+  
+  @Autowired
+  private YamlPropertiesConfig yamlPropertiesConfig;
+  
   @GetMapping("/properties")
-  public String getPropertiesPage() {
+  public String getPropertiesPage(Model model) {
+    model.addAttribute("yamlPropertiesConfig", yamlPropertiesConfig);
     return "/propertiesPage";
   }
 }
